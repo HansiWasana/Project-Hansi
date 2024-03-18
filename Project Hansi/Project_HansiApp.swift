b0d6514ce7d3ff329f9c75a2d1fcbfba10061dd5
HansiWasana <wasanahansi99@gmail.com>
2024-03-17 22:43:17 -0700

    Initial Commit

---
+++ Project Hansi/Project_HansiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_HansiApp.swift
+//  Project Hansi
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Project_HansiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project HansiUITests/Project_HansiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project_HansiUITestsLaunchTests.swift
+//  Project HansiUITests
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-17.
+//
+
+import XCTest
+
+final class Project_HansiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
