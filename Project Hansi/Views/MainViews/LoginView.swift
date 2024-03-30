2ef21708fed803b5b4463fbb5c312d89673fab8d
HansiWasana <wasanahansi99@gmail.com>
2024-03-30 16:03:33 +0530

    Login/Registration

---
+++ Project Hansi/Views/MainViews/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  Project Hansi
+//
+//  Created by NIBM-LAB04-PC15 on 2024-03-30.
+//
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+    
+struct LoginView_Previews: PreviewProvider {
+        static var previews: some View {
+            LoginView()
+            
+        }
+    }
+    
+
---
+++ Project Hansi/Views/MainViews/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Project Hansi
+//
+//  Created by NIBM-LAB04-PC15 on 2024-03-30.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
